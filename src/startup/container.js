f789e7b9cb60f1fd64c49ae8b9ec8f778c0d2dbd
sassenagh <anna.alcaide.hijano@gmail.com>
2020-07-01 18:47:50 +0200

    client services and controllers

--- src/startup/container.js
+++ src/startup/container.js
@@ -1,14 +1,19 @@
 const {createContainer, asClass, asValue, asFunction} = require ('awilix');
+const axios = require ('axios').default;
 
 //config
 const config = require ('../config');
 const app = require ('.');
 
 //services
-const {HomeService} = require ('../services');
+const {HomeService, ClientService, PolicyService} = require ('../services');
 
 //controllers
-const {HomeController} = require ('../controllers');
+const {
+  HomeController,
+  ClientController,
+  PolicyController,
+} = require ('../controllers');
 
 //routes
 const {HomeRoutes} = require ('../routes/index.routes');
@@ -24,12 +29,23 @@ container
   })
   .register ({
     HomeService: asClass (HomeService).singleton (),
+    ClientService: asClass (ClientService).singleton (),
+    PolicyService: asClass (PolicyService).singleton (),
   })
   .register ({
     HomeController: asClass (HomeController.bind (HomeController)).singleton (),
+    ClientController: asClass (
+      ClientController.bind (ClientController)
+    ).singleton (),
+    PolicyController: asClass (
+      PolicyController.bind (PolicyController)
+    ).singleton (),
   })
   .register ({
     HomeRoutes: asFunction (HomeRoutes).singleton (),
+  })
+  .register ({
+    HttpClient: asValue (axios),
   });
 
 module.exports = container;
