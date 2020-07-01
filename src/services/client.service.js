564fb39898fa39ada09994dd10696df935d56a1a
sassenagh <anna.alcaide.hijano@gmail.com>
2020-07-01 20:04:15 +0200

    client fixed

--- src/services/client.service.js
+++ src/services/client.service.js
@@ -23,34 +23,6 @@ class ClientService {
 
     return users.find (user => user.name === username);
   }
-}
-
-module.exports = ClientService;
-let _httpClient = null;
-const {CLIENTS_DATA} = require ('../config');
-
-class ClientService {
-  constructor({HttpClient}) {
-    _httpClient = HttpClient;
-  }
-
-  async getUsers () {
-    const {data: {clients}} = await _httpClient.get (CLIENTS_DATA);
-
-    return clients;
-  }
-
-  async getClientByUserId (id) {
-    const users = await this.getUsers ();
-
-    return users.find (user => user.id === id);
-  }
-
-  async getClientByUsername (name) {
-    const users = await this.getUsers ();
-
-    return users.find (user => user.name === name);
-  }
 
   async getClientByEmail (email) {
     const users = await this.getUsers ();
